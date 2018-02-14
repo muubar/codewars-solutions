8b36e6f270ae8a72f349b4e00e125d11f8d3382b
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-02-15 01:40:47 +0200

    solve which are in

---
+++ Which are in.js
@@ -0,0 +1,14 @@
+function inArray(array1, array2) {
+  var result = array1.filter(function (elem) {
+    for (let i = 0; i < array2.length; i++) {
+      if (array2[i].includes(elem)) return true;
+    }
+    return false;
+  }
+  ).sort();
+  return result;
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> a77783b... solve which are in
